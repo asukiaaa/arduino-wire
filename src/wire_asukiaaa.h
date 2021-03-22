99fac531d5fe9c4d336fc31946b3ce5fd2c7ccb4
Asuki Kono <asukiaaa@gmail.com>
2021-03-22 22:36:42 +0900

    Use hpp and template format

--- src/wire_asukiaaa.h
+++ src/wire_asukiaaa.h
@@ -1,30 +1,5 @@
 #ifndef _WIRE_ASUKIAAA_H_
 #define _WIRE_ASUKIAAA_H_
-
-#include <Arduino.h>
-#include <Wire.h>
-
-namespace wire_asukiaaa {
-  int readBytes(TwoWire *wire, uint8_t deviceAddress, uint8_t registerAddress, uint8_t* data, uint8_t dataLen);
-
-  int writeBytes(TwoWire *wire, uint8_t deviceAddress, uint8_t registerAddress, uint8_t* data, uint8_t dataLen);
-
-  class PeripheralHandler {
-  public:
-    PeripheralHandler(TwoWire* wire, int buffLen, bool (*prohibitWriting)(int index) = NULL);
-    ~PeripheralHandler();
-    void onReceive(int);
-    void onRequest();
-    uint8_t* buffs;
-    const int buffLen;
-    unsigned long receivedAt;
-    int receivedLen;
-
-  private:
-    TwoWire* wire;
-    int buffIndex;
-    bool (*prohibitWriting)(int index);
-  };
-}
+#include "wire_asukiaaa.hpp"
 
 #endif
---
+++ src/wire_asukiaaa.hpp
@@ -0,0 +1,97 @@
+#ifndef _WIRE_ASUKIAAA_HPP_
+#define _WIRE_ASUKIAAA_HPP_
+
+#include <Arduino.h>
+#include <Wire.h>
+
+namespace wire_asukiaaa {
+
+template <class TemplateWire>
+int readBytes(TemplateWire* wire, uint8_t deviceAddress,
+              uint8_t registerAddress, uint8_t* data, uint8_t dataLen) {
+  wire->beginTransmission(deviceAddress);
+  wire->write(registerAddress);
+  uint8_t result = wire->endTransmission(false);
+  if (result != 0) {
+    return result;
+  }
+
+  wire->requestFrom(deviceAddress, dataLen);
+  uint8_t index = 0;
+  while (wire->available()) {
+    uint8_t d = wire->read();
+    if (index < dataLen) {
+      data[index++] = d;
+    }
+  }
+  return 0;
+}
+
+template <class TemplateWire>
+int writeBytes(TemplateWire* wire, uint8_t deviceAddress,
+               uint8_t registerAddress, uint8_t* data, uint8_t dataLen) {
+  wire->beginTransmission(deviceAddress);
+  wire->write(registerAddress);
+  wire->write(data, dataLen);
+  return wire->endTransmission();
+}
+
+template <class TemplateWire = TwoWire>
+class PeripheralHandlerBase {
+ public:
+  uint8_t* buffs;
+  const int buffLen;
+  unsigned long receivedAt;
+  int receivedLen;
+
+  PeripheralHandlerBase(TemplateWire* wire, int buffLen,
+                        bool (*prohibitWriting)(int index) = NULL)
+      : buffLen{buffLen} {
+    this->wire = wire;
+    this->prohibitWriting = prohibitWriting;
+    buffs = new uint8_t[buffLen];
+    for (int i = 0; i < buffLen; ++i) {
+      buffs[0] = 0;
+    }
+    receivedLen = 0;
+    receivedAt = 0;
+  }
+
+  ~PeripheralHandlerBase() { delete[] buffs; }
+
+  void onReceive(int) {
+    receivedLen = 0;
+    while (0 < wire->available()) {
+      uint8_t v = wire->read();
+      if (receivedLen == 0) {
+        buffIndex = v;
+      } else {
+        if (buffIndex < buffLen &&
+            (prohibitWriting == NULL || !prohibitWriting(buffIndex))) {
+          buffs[buffIndex] = v;
+        }
+        ++buffIndex;
+      }
+      ++receivedLen;
+    }
+    receivedAt = millis();
+  }
+
+  void onRequest() {
+    if (buffIndex < buffLen) {
+      wire->write(&buffs[buffIndex], buffLen - buffIndex);
+    } else {
+      wire->write(0);
+    }
+  }
+
+ private:
+  TemplateWire* wire;
+  int buffIndex;
+  bool (*prohibitWriting)(int index);
+};
+
+typedef PeripheralHandlerBase<TwoWire> PeripheralHandler;
+} // namespace wire_asukiaaa
+
+#endif
